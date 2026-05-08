6db2806d5ed13fdeae235fac6064d94d04342fab
SENAI-Itumbiara <ricardotirone@gmail.com>
2026-05-08 15:24:48 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// Este arquivo NÃO deve ser enviado para o GitHub
+const firebaseConfig = {
+    apiKey: "AIzaSyBxd1rJow5FpHKd37IqE6ZG6jmN0ys4Iws", 
+    authDomain: "login-github-pages.firebaseapp.com",
+    projectId: "login-github-pages",
+    storageBucket: "login-github-pages.firebasestorage.app",
+    messagingSenderId: "432460559266",
+    appId: "1:432460559266:web:9578f281439a9be78e38fb"
+};
+
+export default firebaseConfig;
